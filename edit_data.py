dc443e6dc59d37597f4db80e35f880512f1d1ad0
Maike Herzog <maike.herzog@tuni.fi>
2024-04-05 20:33:20 +0200

    Add function to edit data

---
+++ edit_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def edit_data(path_from, path_to):
+    # Open the .data file for reading
+    with open(path_from, 'r') as f:
+        lines = f.readlines()
+
+    # Create a new list for the edited lines
+    cleaned_lines = []
+
+    for line in lines:
+        # Check for '?' in the line
+        if '?' not in line:
+            cleaned_lines.append(line.strip())
+
+    # Column labelling
+    header = "age, workclass, fnlwgt, education, education-num, marital-status	, occupation , relationship, race, sex, capital-gain, capital-loss, hours-per-week, native-country, income"
+
+    # add header and empty line
+    cleaned_data = [header] + cleaned_lines
+
+    # Write the cleaned data to a new file
+    with open(path_to, 'w') as f:
+        for line in cleaned_data:
+            f.write(line + '\n')
+
+    print(f"Clean-up completed and the data was written to a new file with the path {path_to}.")
+
