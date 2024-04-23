d00358e935b4928ab3ad498d61203573f6cf8844
Maike Herzog <maike.herzog@tuni.fi>
2024-04-23 11:34:53 +0200

    InfoLoss functions for continuos data

---
+++ src/castle.py
@@ -0,0 +1,46 @@
+from src.edit_data import attribute_properties
+
+
+def Enlargement(cluster, tupel):
+    """
+      This function calculates the enlargement of a cluster C with respect to a tuple t.
+
+      Args:
+          cluster: A cluster represented as a list of tuples.
+          tupel: A tuple.
+
+      Returns:
+          A list representing the enlargement of C with respect to t.
+      """
+    n = len(cluster[0])  # Assuming all tuples in C have the same length
+    result = [0] * n
+
+    # Calculate VInfoLoss for each attribute
+    for i in range(n):
+        # TODO: Implement VInfoLoss function here
+        pass
+        #vi_loss_c = VInfoLoss(C, i)
+        #vi_loss_t = VInfoLoss([t], i)
+        #result[i] = vi_loss_c - vi_loss_t
+
+    return result
+    pass
+
+def VInfoLoss(attribut, pos) -> int:
+    enlargement = 0
+    if attribute_properties[pos]['type'] == 'continuous':
+        enlargement=VInfoLoss_continuos(attribut, attribute_properties[pos]['interval'])
+    elif attribute_properties[pos]['type'] == 'cathegorical':
+        enlargement= VInfoLoss_cathegorical(attribut, attribute_properties[pos]['hierarchy_tree'])
+    return enlargement
+
+def VInfoLoss_continuos(attribut_range, domain_range):
+    if len(attribut_range) == 1:
+        # TODO: hier nochmal schauen ob das stimmt
+        return (attribut_range[0])/(domain_range[1]-domain_range[0])
+    return (attribut_range[1]- attribut_range[0])/(domain_range[1]-domain_range[0])
+
+def VInfoLoss_cathegorical(attribut_range, domain_range):
+    enlargement = 0
+    return enlargement
+
