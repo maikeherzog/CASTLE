e0df6b62826e22a7e197ddd9e8be89a99aeabffd
Maike Herzog <maike.herzog@tuni.fi>
2024-04-23 11:32:53 +0200

    hierarchy trees as dict

--- src/hierarchy_tree.py
+++ src/hierarchy_tree.py
@@ -1,207 +1,312 @@
 from Node import *
 
-relationship_tree: Node = Node("Any Relationship", [
-    Node("Romantic-partner", [
-        Node("Wife"),
-        Node("Husband")
-    ]),
-    Node("Family-member", [
-        Node("Own-child"),
-        Node("Other-relative")
-    ]),
-    Node("Non-family", [
-        Node("Not-in-family"),
-        Node("Unmarried")
-    ])
-])
+relationship_tree = {
+    'name': 'Any Relationship',
+    'children': [
+        {
+            'name': 'Romantic-partner',
+            'children': [
+                {'name': 'Wife'},
+                {'name': 'Husband'}
+            ]
+        },
+        {
+            'name': 'Family-member',
+            'children': [
+                {'name': 'Own-child'},
+                {'name': 'Other-relative'}
+            ]
+        },
+        {
+            'name': 'Non-family',
+            'children': [
+                {'name': 'Not-in-family'},
+                {'name': 'Unmarried'}
+            ]
+        }
+    ]
+}
 
-education_tree: Node = Node("Any Education", [
-    Node("Preschool"),
-    Node("Elementary School",[
-        Node("1st-4th"),
-        Node("5th-6th"),
-        Node("7th-8th")
-    ]),
-    Node("High School",[
-        Node("9th"),
-        Node("10th"),
-        Node("11th"),
-        Node("12th"),
-        Node("HS-grad")
-    ]),
-    Node("College",[
-        Node("Bachelors"),
-        Node("Some-college"),
-        Node("Prof-school"),
-        Node("Assoc-acdm"),
-        Node("Assoc-voc"),
-        Node("Masters"),
-        Node("Doctorate")
-    ])
-])
+education_tree_dict = {
+    'name': 'Any Education',
+    'children': [
+        {'name': 'Preschool'},
+        {
+            'name': 'Elementary School',
+            'children': [
+                {'name': '1st-4th'},
+                {'name': '5th-6th'},
+                {'name': '7th-8th'}
+            ]
+        },
+        {
+            'name': 'High School',
+            'children': [
+                {'name': '9th'},
+                {'name': '10th'},
+                {'name': '11th'},
+                {'name': '12th'},
+                {'name': 'HS-grad'}
+            ]
+        },
+        {
+            'name': 'College',
+            'children': [
+                {'name': 'Bachelors'},
+                {'name': 'Some-college'},
+                {'name': 'Prof-school'},
+                {'name': 'Assoc-acdm'},
+                {'name': 'Assoc-voc'},
+                {'name': 'Masters'},
+                {'name': 'Doctorate'}
+            ]
+        }
+    ]
+}
 
-marital_status_tree: Node = Node("Any marital-status",[
-    Node("married",[
-        Node("Married-civ-spouse"),
-        Node("Married-spouse-absent"),
-        Node("Married-AF-spouse")
-    ]),
-    Node("not married",[
-        Node("Never-married"),
-        Node("Separated"),
-        Node("Widowed"),
-        Node("Divorced")
-    ])
-])
+marital_status_tree_dict = {
+    'name': 'Any marital-status',
+    'children': [
+        {
+            'name': 'married',
+            'children': [
+                {'name': 'Married-civ-spouse'},
+                {'name': 'Married-spouse-absent'},
+                {'name': 'Married-AF-spouse'}
+            ]
+        },
+        {
+            'name': 'not married',
+            'children': [
+                {'name': 'Never-married'},
+                {'name': 'Separated'},
+                {'name': 'Widowed'},
+                {'name': 'Divorced'}
+            ]
+        }
+    ]
+}
 
-workclass_tree: Node = Node("Any Workclass",[
-    Node("Private"),
-    Node("Self-Employed",[
-        Node("Self-emp-not-inc"),
-        Node("Self-emp-inc")
-    ]),
-    Node("Government",[
-        Node("Federal-gov"),
-        Node("Local-gov"),
-        Node("State-gov")
-    ]),
-    Node("No Pay",[
-        Node("Without-pay"),
-        Node("Never-worked")
-    ])
-])
+workclass_tree_dict = {
+    'name': 'Any Workclass',
+    'children': [
+        {'name': 'Private'},
+        {
+            'name': 'Self-Employed',
+            'children': [
+                {'name': 'Self-emp-not-inc'},
+                {'name': 'Self-emp-inc'}
+            ]
+        },
+        {
+            'name': 'Government',
+            'children': [
+                {'name': 'Federal-gov'},
+                {'name': 'Local-gov'},
+                {'name': 'State-gov'}
+            ]
+        },
+        {
+            'name': 'No Pay',
+            'children': [
+                {'name': 'Without-pay'},
+                {'name': 'Never-worked'}
+            ]
+        }
+    ]
+}
 
-race_tree: Node = Node("Any Race",[
-    Node("White"),
-    Node("Asian-Pac-Islander"),
-    Node("Amer-Indian-Eskimo"),
-    Node("Other"),
-    Node("Black")
-])
+sex_tree_dict = {
+    'name': 'Any Sex',
+    'children': [
+        {'name': 'Female'},
+        {'name': 'Male'}
+    ]
+}
 
-sex_tree: Node = Node("Any Sex",[
-    Node("Female"),
-    Node("Male")
-])
 
-native_country_tree: Node = Node("Any native country",[
-    Node("Amerika",[
-        Node("North America",[
-            Node("United-States"),
-            Node("Canada"),
-            Node("Mexico")
-        ]),
-        Node("Central America",[
-            Node("Guatemala"),
-            Node("Honduras"),
-            Node("Nicaragua"),
-            Node("El-Salvador")
-        ]),
-        Node("South America",[
-            Node("Peru"),
-            Node("Colombia"),
-            Node("Ecuador"),
-            Node("Bolivia")
-        ]),
-        Node("Caribbean", [
-            Node("Puerto-Rico"),
-            Node("Cuba"),
-            Node("Jamaica"),
-            Node("Haiti"),
-            Node("Dominican-Republic"),
-            Node("Trinadad&Tobago")
-        ])
-    ]),
-    Node("Europe",[
-        Node("Western Europe",[
-            Node("England"),
-            Node("Germany"),
-            Node("France"),
-            Node("Italy"),
-            Node("Spain"),
-            Node("Portugal"),
-            Node("Ireland"),
-            Node("Scotland"),
-            Node("Netherlands")
-        ]),
-        Node("Eastern Europe",[
-            Node("Poland"),
-            Node("Hungary"),
-            Node("Yugoslavia")
-        ]),
-        Node("Northern Europe",[
-            Node("Sweden"),
-            Node("Norway"),
-            Node("Finland"),
-            Node("Denmark")
-        ]),
-        Node("Southern Europe",[
-            Node("Greece"),
-            Node("Italy"),
-            Node("Portugal"),
-            Node("Spain"),
-            Node("France")
-    ])
-    ]),
-    Node("Asia",[
-        Node("Eastern Asia",[
-            Node("Japan"),
-            Node("China"),
-            Node("South Korea"),
-            Node("Taiwan"),
-            Node("Vietnam")
-        ]),
-        Node("South Asia",[
-            Node("India"),
-            Node("Pakistan"),
-            Node("Bangladesh"),
-            Node("Sri Lanka")
-        ]),
-        Node("Southeast Asia",[
-            Node("Philippines"),
-            Node("Thailand"),
-            Node("Cambodia"),
-            Node("Laos")
-        ]),
-        Node("Middle East",[
-            Node("Iran"),
-            Node("Iraq"),
-            Node("Israel"),
-            Node("Saudi-Arabia"),
-            Node("Turkey")
-        ])
-    ]),
-    Node("Oceania",[
-        Node("Australia"),
-        Node("New-Zealand"),
-        Node("Guam"),
-        Node("Fiji"),
-        Node("Papua-New-Guinea")
-])
-])
 
-occupation_tree: Node = Node("Any occupation",[
-    Node("Service",[
-        Node("Tech-support"),
-        Node("Craft-repair"),
-        Node("Other-service"),
-        Node("Priv-house-serv")
-    ]),
-    Node("Sales"),
-    Node("Exec-managerial"),
-    Node("Prof-specialty"),
-    Node("Adm-clerical"),
-    Node("Labor",[
-        Node("Handlers-cleaners"),
-        Node("Machine-op-inspct"),
-        Node("Farming-fishing"),
-        Node("Transport-moving")
-    ]),
-    Node("Protective services",[
-        Node("Protective-serv"),
-        Node("Armed-Forces")
-    ])
-])
+native_country_tree_dict = {
+    'name': 'Any native country',
+    'children': [
+        {
+            'name': 'Amerika',
+            'children': [
+                {
+                    'name': 'North America',
+                    'children': [
+                        {'name': 'United-States'},
+                        {'name': 'Canada'},
+                        {'name': 'Mexico'}
+                    ]
+                },
+                {
+                    'name': 'Central America',
+                    'children': [
+                        {'name': 'Guatemala'},
+                        {'name': 'Honduras'},
+                        {'name': 'Nicaragua'},
+                        {'name': 'El-Salvador'}
+                    ]
+                },
+                {
+                    'name': 'South America',
+                    'children': [
+                        {'name': 'Peru'},
+                        {'name': 'Colombia'},
+                        {'name': 'Ecuador'},
+                        {'name': 'Bolivia'}
+                    ]
+                },
+                {
+                    'name': 'Caribbean',
+                    'children': [
+                        {'name': 'Puerto-Rico'},
+                        {'name': 'Cuba'},
+                        {'name': 'Jamaica'},
+                        {'name': 'Haiti'},
+                        {'name': 'Dominican-Republic'},
+                        {'name': 'Trinadad&Tobago'}
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Europe',
+            'children': [
+                {
+                    'name': 'Western Europe',
+                    'children': [
+                        {'name': 'England'},
+                        {'name': 'Germany'},
+                        {'name': 'France'},
+                        {'name': 'Italy'},
+                        {'name': 'Spain'},
+                        {'name': 'Portugal'},
+                        {'name': 'Ireland'},
+                        {'name': 'Scotland'},
+                        {'name': 'Netherlands'}
+                    ]
+                },
+                {
+                    'name': 'Eastern Europe',
+                    'children': [
+                        {'name': 'Poland'},
+                        {'name': 'Hungary'},
+                        {'name': 'Yugoslavia'}
+                    ]
+                },
+                {
+                    'name': 'Northern Europe',
+                    'children': [
+                        {'name': 'Sweden'},
+                        {'name': 'Norway'},
+                        {'name': 'Finland'},
+                        {'name': 'Denmark'}
+                    ]
+                },
+                {
+                    'name': 'Southern Europe',
+                    'children': [
+                        {'name': 'Greece'},
+                        {'name': 'Italy'},
+                        {'name': 'Portugal'},
+                        {'name': 'Spain'},
+                        {'name': 'France'}
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Asia',
+            'children': [
+                {
+                    'name': 'Eastern Asia',
+                    'children': [
+                        {'name': 'Japan'},
+                        {'name': 'China'},
+                        {'name': 'South Korea'},
+                        {'name': 'Taiwan'},
+                        {'name': 'Vietnam'}
+                    ]
+                },
+                {
+                    'name': 'South Asia',
+                    'children': [
+                        {'name': 'India'},
+                        {'name': 'Pakistan'},
+                        {'name': 'Bangladesh'},
+                        {'name': 'Sri Lanka'}
+                    ]
+                },
+                {
+                    'name': 'Southeast Asia',
+                    'children': [
+                        {'name': 'Philippines'},
+                        {'name': 'Thailand'},
+                        {'name': 'Cambodia'},
+                        {'name': 'Laos'}
+                    ]
+                },
+                {
+                    'name': 'Middle East',
+                    'children': [
+                        {'name': 'Iran'},
+                        {'name': 'Iraq'},
+                        {'name': 'Israel'},
+                        {'name': 'Saudi-Arabia'},
+                        {'name': 'Turkey'}
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Oceania',
+            'children': [
+                {'name': 'Australia'},
+                {'name': 'New-Zealand'},
+                {'name': 'Guam'},
+                {'name': 'Fiji'},
+                {'name': 'Papua-New-Guinea'}
+            ]
+        }
+    ]
+}
+
+occupation_tree_dict = {
+    'name': 'Any occupation',
+    'children': [
+        {
+            'name': 'Service',
+            'children': [
+                {'name': 'Tech-support'},
+                {'name': 'Craft-repair'},
+                {'name': 'Other-service'},
+                {'name': 'Priv-house-serv'}
+            ]
+        },
+        {'name': 'Sales'},
+        {'name': 'Exec-managerial'},
+        {'name': 'Prof-specialty'},
+        {'name': 'Adm-clerical'},
+        {
+            'name': 'Labor',
+            'children': [
+                {'name': 'Handlers-cleaners'},
+                {'name': 'Machine-op-inspct'},
+                {'name': 'Farming-fishing'},
+                {'name': 'Transport-moving'}
+            ]
+        },
+        {
+            'name': 'Protective services',
+            'children': [
+                {'name': 'Protective-serv'},
+                {'name': 'Armed-Forces'}
+            ]
+        }
+    ]
+}
 
 
 
