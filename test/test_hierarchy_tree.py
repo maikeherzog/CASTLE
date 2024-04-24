b72f448b9142b448f941e7db1d1f93f9ff59c4a5
Maike Herzog <maike.herzog@tuni.fi>
2024-04-24 09:20:47 +0200

    edit tests for generalization

--- test/test_hierarchy_tree.py
+++ test/test_hierarchy_tree.py
@@ -1,38 +1,35 @@
 from src.hierarchy_tree import *
+from src.tree_functions import find_generalization
+
 
 def test_generalization_romantic_partner():
     attributes = ["Wife", "Husband"]
-    generalization = relationship_tree.find_generalization(attributes)
+    generalization = find_generalization(relationship_tree, attributes)
     assert generalization == "Romantic-partner"
 
 def test_generalization_any_relationship():
     attributes = ["Wife", "Own-child"]
-    generalization = relationship_tree.find_generalization(attributes)
+    generalization = find_generalization(relationship_tree, attributes)
     assert generalization == "Any Relationship"
 
 def test_generalization_own_child():
     attributes = ["Own-child"]
-    generalization = relationship_tree.find_generalization(attributes)
+    generalization = find_generalization(relationship_tree, attributes)
     assert generalization == "Own-child"
-
-def test_generalization_non_existent():
-    attributes = ["Wife", "Brother"]
-    generalization = relationship_tree.find_generalization(attributes)
-    assert generalization is None
 
 def test_generalization_duplicate_attributes():
     attributes = ["Wife", "Husband", "Wife"]
-    generalization = relationship_tree.find_generalization(attributes)
+    generalization = find_generalization(relationship_tree, attributes)
     assert generalization == "Romantic-partner"
 
 def test_generalization_all_family():
     attributes = ["Own-child", "Other-relative"]
-    generalization = relationship_tree.find_generalization(attributes)
+    generalization = find_generalization(relationship_tree, attributes)
     assert generalization == "Family-member"
 
 def test_generalization_non_unique_match():
     attributes = ["Wife", "Husband", "Own-child"]
-    generalization = relationship_tree.find_generalization(attributes)
+    generalization = find_generalization(relationship_tree, attributes)
     assert generalization == "Any Relationship"
 
 
