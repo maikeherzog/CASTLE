3501c2a78094dd4f32bbc578735cf3b1198af776
Maike Herzog <maike.herzog@tuni.fi>
2024-04-25 11:06:50 +0200

    implement Information Loss

--- src/castle.py
+++ src/castle.py
@@ -1,4 +1,4 @@
-from src.edit_data import attribute_properties
+from src.edit_data import attribute_properties, attribute_properties_test
 from src.tree_functions import *
 
 
@@ -11,28 +11,63 @@ def Enlargement(cluster, tupel):
           tupel: A tuple.
 
       Returns:
-          A list representing the enlargement of C with respect to t.
+          A number representing the enlargement of a cluster with respect to t.
       """
-    n = len(cluster[0])  # Assuming all tuples in C have the same length
-    result = [0] * n
+    n = len(cluster)  # Assuming all tuples in C have the same length
+    enlarged_cluster = add_tupel(cluster, tupel)
+    sum_iloss_cluster = 0
+    sum_iloss_enlarged_cluster = 0
 
     # Calculate VInfoLoss for each attribute
     for i in range(n):
-        # TODO: Implement VInfoLoss function here
-        pass
-        #vi_loss_c = VInfoLoss(C, i)
-        #vi_loss_t = VInfoLoss([t], i)
-        #result[i] = vi_loss_c - vi_loss_t
+        info_loss_cluster = VInfoLoss_cluster(cluster[i], i)
+        sum_iloss_cluster += info_loss_cluster
+        info_loss_together = VInfoLoss(enlarged_cluster[i], i)
+        sum_iloss_enlarged_cluster += info_loss_together
 
-    return result
-    pass
+    return 1/n * sum_iloss_enlarged_cluster - 1/n * sum_iloss_cluster
 
+def add_tupel(cluster, tupel):
+    new_cluster = list(cluster)
+    for i in range(len(cluster)-1):
+        if attribute_properties_test[i]['type'] == 'continuous':
+            new_cluster[i] = adjust_interval(tupel[i], cluster[i])
+
+        elif attribute_properties_test[i]['type'] == 'cathegorical':
+            new_cluster[i] = add_unique_string_to_list(tupel[i], cluster[i])
+
+    return tuple(new_cluster)
+
+def adjust_interval(value, interval):
+    lower_bound, upper_bound = interval
+
+    if value < lower_bound:
+        lower_bound = value
+    elif value > upper_bound:
+        upper_bound = value
+
+    return [lower_bound, upper_bound]
+
+def add_unique_string_to_list(value, value_list):
+    if value not in value_list:
+        value_list.append(value)
+def is_in_interval(value, interval):
+    lower_bound, upper_bound = interval
+    return lower_bound <= value <= upper_bound
 def VInfoLoss(attribut, pos) -> int:
     enlargement = 0
-    if attribute_properties[pos]['type'] == 'continuous':
-        enlargement=VInfoLoss_continuos(attribut, attribute_properties[pos]['interval'])
-    elif attribute_properties[pos]['type'] == 'cathegorical':
-        enlargement= VInfoLoss_cathegorical(attribut, attribute_properties[pos]['hierarchy_tree'])
+    if attribute_properties_test[pos]['type'] == 'continuous':
+        enlargement=VInfoLoss_continuos(attribut, attribute_properties_test[pos]['interval'])
+    elif attribute_properties_test[pos]['type'] == 'cathegorical':
+        enlargement= VInfoLoss_cathegorical(attribut, attribute_properties_test[pos]['hierarchy_tree'])
+    return enlargement
+
+def VInfoLoss_cluster(attribut, pos) -> int:
+    enlargement = 0
+    if attribute_properties_test[pos]['type'] == 'continuous':
+        enlargement=VInfoLoss_continuos(attribut, attribute_properties_test[pos]['interval'])
+    elif attribute_properties_test[pos]['type'] == 'cathegorical':
+        enlargement= VInfoLoss_cathegorical_cluster(attribut, attribute_properties_test[pos]['hierarchy_tree'])
     return enlargement
 
 def VInfoLoss_continuos(attribut_range, domain_range):
@@ -45,5 +80,12 @@ def VInfoLoss_cathegorical(attribut_range, domain_tree):
     domain_range = count_all_leaves(domain_tree)
     attribute_range_generalized = find_generalization(domain_tree, attribut_range)
     generalized_range = count_all_leaves(get_subtree(domain_tree, attribute_range_generalized))
-    return generalized_range/domain_range
+    erg = (generalized_range - 1) /(domain_range - 1)
+    return erg
+
+def VInfoLoss_cathegorical_cluster(attribut_range, domain_tree):
+    domain_range = count_all_leaves(domain_tree)
+    generalized_range = len(attribut_range)
+    erg = (generalized_range - 1) /(domain_range - 1)
+    return erg
 
