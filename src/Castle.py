7d88326a6ad6474be859bf16319d3f9aac796b52
Maike Herzog <maike.herzog@tuni.fi>
2024-05-03 09:54:38 +0200

    add best_selection function an first part of Castle_funktion

--- src/Castle.py
+++ src/Castle.py
@@ -1,5 +1,60 @@
+from src.Cluster import Cluster
 from src.edit_data import attribute_properties, attribute_properties_test
 from src.tree_functions import *
+
+# Initialisierung
+not_anonymized_clusters = set()  # Set of non-ks-anonymized clusters der Klasse Cluster
+anonymized_clusters = set()  # Set of ks-anonymized clusters
+tao = 0
+beta = 2
+def Castle(S, k, delta, beta):
+    """
+        Implementierung des CASTLE-Algorithmus.
+
+        Args:
+          S: input stream.
+          k: Der Anonymisierungsgrad.
+          delta: Der Datenschutzparameter.
+          beta: Der Genauigkeitsparameter.
+
+        Returns:
+          Eine Liste der k-anonymisierten Cluster.
+        """
+    while S:
+        next_tupel = S.pop() #Get the next tupel from S
+        best_cluster = best_selection(next_tupel)
+        if best_cluster is None:
+            new_cluster = Cluster(next_tupel)
+            not_anonymized_clusters.add(new_cluster)
+        else:
+            best_cluster.add_tupel(next_tupel)
+
+
+def best_selection(tuple):
+    enlargements = set()
+    if not not_anonymized_clusters:
+        return None
+    # Möglichkeit mit List-Comprehension darstellen:
+    # enlargements = set([Enlargement(cluster, tuple) for cluster in not_anonymized_clusters])
+    for cluster in not_anonymized_clusters:
+        enlargement = Enlargement(cluster.t, tuple)
+        enlargements.add(enlargement)
+    min_enlargement = min(enlargements)
+
+    # Set of clusters C' in not_anonymized_clusters with Enlargement(C', tupel) = min
+    set_cluster_min = [C for C in not_anonymized_clusters if Enlargement(C.t, tuple) == min_enlargement]
+    set_c_ok = set()
+    for cluster in set_cluster_min:
+        IL_cluster = InfoLoss(cluster.t)
+        if IL_cluster >= tao:
+            set_c_ok.add(cluster)
+    if not set_c_ok:
+        if len(not_anonymized_clusters) >= beta:
+            return min(set_cluster_min, key=lambda cluster: len(cluster.data))
+        else:
+            return None
+    else:
+        return min(set_c_ok, key=lambda cluster: len(cluster.data))
 
 
 def Enlargement(cluster, tupel):
@@ -26,6 +81,13 @@ def Enlargement(cluster, tupel):
         sum_iloss_enlarged_cluster += info_loss_together
 
     return 1/n * sum_iloss_enlarged_cluster - 1/n * sum_iloss_cluster
+
+def InfoLoss(tupel):
+    n = len(tupel)
+    sum = 0
+    for att_pos in range(n):
+        sum += VInfoLoss_cluster(tupel[att_pos],att_pos)
+    return 1/n*sum
 
 def add_tupel(cluster, tupel):
     new_cluster = list(cluster)
@@ -55,20 +117,20 @@ def is_in_interval(value, interval):
     lower_bound, upper_bound = interval
     return lower_bound <= value <= upper_bound
 def VInfoLoss(attribut, pos) -> int:
-    enlargement = 0
+    info_loss = 0
     if attribute_properties_test[pos]['type'] == 'continuous':
-        enlargement=VInfoLoss_continuos(attribut, attribute_properties_test[pos]['interval'])
+        info_loss=VInfoLoss_continuos(attribut, attribute_properties_test[pos]['interval'])
     elif attribute_properties_test[pos]['type'] == 'cathegorical':
-        enlargement= VInfoLoss_cathegorical(attribut, attribute_properties_test[pos]['hierarchy_tree'])
-    return enlargement
+        info_loss= VInfoLoss_cathegorical(attribut, attribute_properties_test[pos]['hierarchy_tree'])
+    return info_loss
 
 def VInfoLoss_cluster(attribut, pos) -> int:
-    enlargement = 0
+    info_loss = 0
     if attribute_properties_test[pos]['type'] == 'continuous':
-        enlargement=VInfoLoss_continuos(attribut, attribute_properties_test[pos]['interval'])
+        info_loss=VInfoLoss_continuos(attribut, attribute_properties_test[pos]['interval'])
     elif attribute_properties_test[pos]['type'] == 'cathegorical':
-        enlargement= VInfoLoss_cathegorical_cluster(attribut, attribute_properties_test[pos]['hierarchy_tree'])
-    return enlargement
+        info_loss= VInfoLoss_cathegorical_cluster(attribut, attribute_properties_test[pos]['hierarchy_tree'])
+    return info_loss
 
 def VInfoLoss_continuos(attribut_range, domain_range):
     if len(attribut_range) == 1:
