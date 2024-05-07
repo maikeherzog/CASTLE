6a40bc9ea3d158fcd455660704ced411e54c0db8
Maike Herzog <maike.herzog@tuni.fi>
2024-05-07 14:16:59 +0200

    refactor test_ILoss.py

--- test/test_ILoss.py
+++ test/test_ILoss.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.Castle import Enlargement
+from src.Castle import Castle
 
 education_tree_test = {
     'name': 'Any Education',
@@ -31,25 +31,26 @@ attribute_properties_test = {
 class TestEnlargementFunction(unittest.TestCase):
 
     def test_enlargement(self):
+        castle = Castle(6, 5, 2)
         # testcase 1
         cluster1 = ([26, 28], ['Masters', 'Bachelors'])
         tupel1 = (24, 'Bachelors')
         expected_result1 = 0.13480392156862744
-        result1 = Enlargement(cluster1, tupel1)
+        result1 = castle.Enlargement(cluster1, tupel1)
         self.assertAlmostEqual(result1, expected_result1, places=3)
 
         # testcase 2
         cluster2 = ([18,22], ['Primary School', 'Bachelors'])
         tupel2 = (24, 'Bachelors')
         expected_result2 = 0.38480392156862747
-        result2 = Enlargement(cluster2, tupel2)
+        result2 = castle.Enlargement(cluster2, tupel2)
         self.assertAlmostEqual(result2, expected_result2, places=3)
 
         # testcase 3
         cluster1 = ([24, 28], ['Masters', 'Bachelors'])
         tupel1 = (24, 'Bachelors')
         expected_result1 = 0.125
-        result1 = Enlargement(cluster1, tupel1)
+        result1 = castle.Enlargement(cluster1, tupel1)
         self.assertAlmostEqual(result1, expected_result1, places=3)
 
 
