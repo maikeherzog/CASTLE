ca9daefe47ec1ad741f529d3f9d020d357ec5fee
Maike Herzog <maike.herzog@tuni.fi>
2024-05-23 15:59:20 +0200

    add Tuple structure

---
+++ src/Tupel.py
@@ -0,0 +1,15 @@
+class Tuple:
+  """
+  Ein Datensatz.
+
+  Args:
+    
+
+  """
+
+  def __init__(self, pid, time, qi, sensitive_attributes):
+    self.pid = pid
+    self.time = time
+    self.qi = qi
+    self.sensitive_attributes = sensitive_attributes
+
