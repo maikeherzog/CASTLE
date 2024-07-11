7be399ba0b7757216a06887cdfd950fd1f8863cc
Maike Herzog <maike.herzog@tuni.fi>
2024-07-11 09:48:19 +0200

    update test_Tuple and Tuple.py

--- src/Tupel.py
+++ src/Tupel.py
@@ -33,8 +33,8 @@ class Tuple:
   def set_qi(self, qi):
     self.qi = qi
 
-  def is_equal(self, other):
+  """def is_equal(self, other):
     if self.pid == other.pid & self.time == other.time & self.qi == other.qi & self.sensitive_attributes == other.sensitive_attributes:
       return True
     else:
-        return False
+        return False"""
--- test/test_tuple.py
+++ test/test_tuple.py
@@ -5,16 +5,27 @@ from src.Tupel import Tuple
 
 class TestTuple(unittest.TestCase):
     def test_eq(self):
-        # Erstellen Sie zwei Tuple, die im Sinne Ihrer __eq__ Methode gleich sein sollten
         tuple1 = Tuple(0, 0, (52, 'Secondary School'), ())
         tuple2 = Tuple(0, 0, (52, 'Secondary School'), ())
+        tuple3 = Tuple(1, 0, (52, 'Secondary School'), ())
+
         # Assert that they are equal
         self.assertEqual(tuple1, tuple2)
 
-        # Erstellen Sie zwei Tuple, die unterschiedlich sind
-        tuple3 = Tuple(1, 0, (52, 'Secondary School'), ())
         # Assert that they are not equal
         self.assertNotEqual(tuple1, tuple3)
+
+    def test_len_qi(self):
+        tuple1 = Tuple(0, 0, (52, 'Secondary School'), ())
+        self.assertEqual(tuple1.len_qi(), 2)
+        self.assertNotEqual(tuple1.len_qi(), 3)
+
+    def test_set_qi(self):
+        tuple1 = Tuple(0, 0, (52, 'Secondary School'), ())
+        tuple1.set_qi(([50,55], 'Secondary School'))
+        self.assertEqual(tuple1.qi, ([50,55], 'Secondary School'))
+        self.assertNotEqual(tuple1.qi, (52, 'Primary School'))
+
 
 
 # Ausführen der Test-Suite
