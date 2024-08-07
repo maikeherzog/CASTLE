25d73199f2c10b8d5e823490161a9119af1ec8b6
Maike Herzog <maike.herzog@tuni.fi>
2024-08-07 12:14:59 +0200

    add comments Tupel Class

--- src/Tupel.py
+++ src/Tupel.py
@@ -6,10 +6,10 @@ class Tuple:
     def __init__(self, time, pid, qi, sensitive_attributes):
         """
         Constructor of the Tuple class.
-        :param time:
-        :param pid:
-        :param qi:
-        :param sensitive_attributes:
+        :param time (int): the arrival time of the tuple
+        :param pid (int): the person-ID of the tuple
+        :param qi (tuple): the Quasi-identifier attributes of the tuple
+        :param sensitive_attributes (tuple): the sensitive attributes of the tuple
         """
         self.pid = pid
         self.time = time
@@ -19,8 +19,8 @@ class Tuple:
     def __eq__(self, other):
         """
         Compares two tuples if equal.
-        :param other:
-        :return:
+        :param other (Tuple): other tuple
+        :return: True if self is equal to other, False otherwise
         """
         if isinstance(other, Tuple):
           return (self.pid == other.pid and
@@ -32,15 +32,14 @@ class Tuple:
     def len_qi(self):
         """
         Returns the number of Quasi-identifier attributes.
-        :return:
+        :return: the number of Quasi-identifier attributes
         """
         return len(self.qi)
 
     def set_qi(self, qi):
         """
         Sets the Quasi-identifier attributes.
-        :param qi:
-        :return:
+        :param qi (tuple): the Quasi-identifier attributes
         """
         self.qi = qi
 
