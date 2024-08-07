6df263174411045293bd4854e0803059287169a2
Maike Herzog <maike.herzog@tuni.fi>
2024-08-07 11:53:16 +0200

    comments HeapElement Class

--- src/HeapElement.py
+++ src/HeapElement.py
@@ -1,7 +1,20 @@
 class HeapElement:
+    """
+    Class to represent an element in the heap
+    """
     def __init__(self, dist, tuple):
+        """
+        Constructor of the HeapElement class.
+        :param dist: specifies the distance of the tuple to another element
+        :param tuple: tuple for which the distance is calculated
+        """
         self.dist = dist
         self.tuple = tuple
 
     def __lt__(self, other):
+        """
+        Compares two HeapElements if smaller.
+        :param other: other HeapElement
+        :return: True if self is smaller than other, False otherwise
+        """
         return self.dist < other.dist
