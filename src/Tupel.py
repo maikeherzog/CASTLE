0db189247518e7d8b936e862ce86730aa652e591
Maike Herzog <maike.herzog@tuni.fi>
2024-08-07 11:46:11 +0200

    comments Tuple Class

--- src/Tupel.py
+++ src/Tupel.py
@@ -1,36 +1,47 @@
-from src.edit_data import attribute_properties
-from src.tree_functions import find_generalization
-
-
 class Tuple:
-  """
-  Ein Datensatz.
+    """
+    A class to represent a tuple.
+    """
 
-  Args:
+    def __init__(self, time, pid, qi, sensitive_attributes):
+        """
+        Constructor of the Tuple class.
+        :param time:
+        :param pid:
+        :param qi:
+        :param sensitive_attributes:
+        """
+        self.pid = pid
+        self.time = time
+        self.qi = qi
+        self.sensitive_attributes = sensitive_attributes
 
+    def __eq__(self, other):
+        """
+        Compares two tuples if equal.
+        :param other:
+        :return:
+        """
+        if isinstance(other, Tuple):
+          return (self.pid == other.pid and
+                  self.time == other.time and
+                  self.qi == other.qi and
+                  self.sensitive_attributes == other.sensitive_attributes)
+        return False
 
-  """
+    def len_qi(self):
+        """
+        Returns the number of Quasi-identifier attributes.
+        :return:
+        """
+        return len(self.qi)
 
-  def __init__(self, time, pid, qi, sensitive_attributes):
-    self.pid = pid
-    self.time = time
-    self.qi = qi
-    self.sensitive_attributes = sensitive_attributes
-
-  def __eq__(self, other):
-    if isinstance(other, Tuple):
-      return (self.pid == other.pid and
-              self.time == other.time and
-              self.qi == other.qi and
-              self.sensitive_attributes == other.sensitive_attributes)
-    return False
-
-  def __ne__(self, other):
-    return not self.__eq__(other)
-  def len_qi(self):
-    return len(self.qi)
-
-  def set_qi(self, qi):
-    self.qi = qi
+    def set_qi(self, qi):
+        """
+        Sets the Quasi-identifier attributes.
+        :param qi:
+        :return:
+        """
+        self.qi = qi
 
 
