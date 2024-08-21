8f4d71cb4544e4ff34dee3832d724b7f52edcbf6
Maike Herzog <maike.herzog@tuni.fi>
2024-08-21 14:42:54 +0200

    language comments english

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import pandas as pd
 from src.Castle import Castle
 from src.Data import Data
 
-# Sortieren der Daten
+# Sorting the data
 #edit_data_sorted("C:/Users/maike/Documents/TU Dresden/Bachelorarbeit/CASTLE/data/adult.data", "C:/Users/maike/Documents/TU Dresden/Bachelorarbeit/CASTLE/data/adult_sorted.csv")
 
 
-# Pfad zur CSV-Datei
+# Path to the CSV file
 csv_datei_pfad_castle = 'data/adult_castle.csv'
 csv_datei_pfad_reverse = 'data/adult_castle_reverse.csv'
 csv_datei_pfad_mix = 'data/adult_castle_mix.csv'
 
 
-# Anzahl der Zeilen, die eingelesen werden sollen
+# Number of lines to be read in
 anzahl_zeilen = 32000
 
-# CSV-Datei mit pandas einlesen
+# Import CSV file with pandas
 df_castle = pd.read_csv(csv_datei_pfad_castle, nrows=anzahl_zeilen)
 df_reverse = pd.read_csv(csv_datei_pfad_reverse, nrows=anzahl_zeilen)
 df_mix = pd.read_csv(csv_datei_pfad_mix, nrows=anzahl_zeilen)
 
-# Dataframe in Liste von Tupeln umwandeln
+# Convert dataframe to list of tuples
 print(df_castle)
 data_tuples_castle = list(df_castle.itertuples(index=False, name=None))
 print(data_tuples_castle)
@@ -34,18 +34,18 @@ print(df_mix)
 data_tuples_mix = list(df_mix.itertuples(index=False, name=None))
 print(data_tuples_mix)
 
-# original für IL und k
+# IL and k (adult_castle Dataset)
 
-#Bestimmen der QIs und der sensiblen Attribute
+# Specifying the QIs and sensitive attributes
 data = Data(data_tuples_castle, [2, 12], [])
 
-#erstellen der CASTLE Instanzen mit den Parametern
+# Create the CASTLE instances with the parameters
 castle = Castle(data.data, 200, 10000, 50, "adult_castle")
-#ausführen des CASTLE Algorithmus
+# Execute the CASTLE algorithm
 castle.castle_algo(data.data)
 print("Average InfoLoss: ", castle.average_Loss_all())
 
-#wiederholen mit anderen Parametern
+# Repeat with other parameters
 castle = Castle(data.data, 100, 10000, 50, "adult_castle")
 castle.castle_algo(data.data)
 print("Average InfoLoss: ", castle.average_Loss_all())
@@ -58,21 +58,21 @@ castle = Castle(data.data, 20, 10000, 50, "adult_castle")
 castle.castle_algo(data.data)
 print("Average InfoLoss: ", castle.average_Loss_all())
 
-#iLoss und QI
-#Bestimmen der QIs und der sensiblen Attribute
+# ILoss und QI
+# Specifying the QIs and sensitive attributes
 data1_castle = Data(data_tuples_castle, [2,4], [])
 data2_castle = Data(data_tuples_castle, [2,6], [])
 data3_castle= Data(data_tuples_castle, [2, 8], [])
 data4_castle = Data(data_tuples_castle, [2, 10], [])
 data5_castle = Data(data_tuples_castle, [2, 12], [])
 
-#erstellen der CASTLE Instanzen mit den Parametern
+# Create the CASTLE instances with the parameters
 castle1 = Castle(data1_castle.data, 100, 10000, 50, "adult_castle")
-#ausführen des CASTLE Algorithmus
+# Execute the CASTLE algorithm
 castle1.castle_algo(data1_castle.data)
 print("Average InfoLoss: ", castle1.average_Loss())
 
-#wiederholen mit anderen QI-Mengen
+# Repeat with other QI sets
 castle2 = Castle(data2_castle.data, 100, 10000, 50, "adult_castle")
 castle2.castle_algo(data2_castle.data)
 print("Average InfoLoss: ", castle2.average_Loss())
